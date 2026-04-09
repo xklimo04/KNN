246584446e6dbbc6bb202498b5ec40c4285a8f5e
xklimo04 <xklimo04@vutbr.cz>
2026-04-09 14:07:30 +0200

    feat: freeze encoder when laoding the pretrained model

--- prepare_pretrained_model.py
+++ prepare_pretrained_model.py
@@ -9,12 +9,21 @@ model_name = "microsoft/trocr-small-printed"
 processor = TrOCRProcessor.from_pretrained(model_name)
 model = VisionEncoderDecoderModel.from_pretrained(model_name)
 
+# Freeze encoder
+for param in model.encoder.parameters():
+    param.requires_grad = False
+
 # Add new tokens to tokenizer
 with open("./datasets/im2latex-100k-norm/vocab.json", "r") as f:
     new_tokens = json.load(f)
 added_tokens = processor.tokenizer.add_tokens(new_tokens)
 print(f"Added {added_tokens} new tokens to the tokenizer.")
 model.decoder.resize_token_embeddings(len(processor.tokenizer))
+
+# Set special tokens
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+model.config.eos_token_id = processor.tokenizer.sep_token_id
 
 # Save model
 processor.save_pretrained("./models/trocr_processor")
